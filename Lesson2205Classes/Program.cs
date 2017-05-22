35a7ca8f08f5ee7fac3af0605a1555e1453aae8d
Oleksandr <xander_uzzzer@mail.ru>
2017-05-22 21:09:16 +0300

    2109

--- Lesson2205Classes/Program.cs
+++ Lesson2205Classes/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace Lesson2205Classes
 {
@@ -11,8 +13,40 @@ namespace Lesson2205Classes
         static void Main(string[] args)
         {
             Console.WriteLine("Lesson 2205 classes");
+            //Console.WriteLine(string.Join("\n", typeof(System.IO.File)
+            //    .GetMethods(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public)
+            //    .Select(f => f.Name)
+            //    .Where(f => Regex.Matches(f, "[A-Z][a-z]*").Count> 2)));
+            int[] arr = new int[100000000];
+            Random r = new Random();
+            int s = 0;
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = r.Next(0, 20);
+            ThreadTest t = new ThreadTest();
+            Thread thread = new Thread(new ParameterizedThreadStart(Sum(t.arr, s)));
 
+
+            Console.WriteLine("over");
             Console.ReadLine();
+
+        }
+        static void Sum(int[] arr, int s)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                s += arr[i];
         }
     }
+    class ThreadTest
+    {
+        public int[] arr = new int[100000000];
+        Random r = new Random();
+        public int s = 0;
+
+        public ThreadTest()
+        {
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = r.Next(0, 20);
+        }
+    }
+
 }
---
+++ Lesson2205Classes/TaskPlanner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lesson2205Classes
+{
+    class TaskPlanner
+    {
+        private class Job
+        {
+            public int Delay;
+            public Action Action;
+        }
+        private int quant;
+        private List<Job> actionsToDo = new List<Job>();
+        public TaskPlanner(int quant)
+        {
+
+        }
+
+        public void SheduleLJob()
+        {
+
+        }
+
+        public void Work()
+        {
+            while (true)
+            {
+                Thread.Sleep(quant);
+                foreach (var item in actionsToDo)
+                    item.Delay -= quant;
+            }
+        }
+    }
+}
